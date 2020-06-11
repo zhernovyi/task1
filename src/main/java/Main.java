bce1c5cb515da83c42be5fd762649f68759d9d47
zhernovii <44294650+zhernovii@users.noreply.github.com>
2020-06-11 20:09:02 +0300

    second commit(2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        System.out.print((char)27+"[30m");
-        //new info;
         Scanner sc = new Scanner(System.in);
         int positiveTrying = 5;
         int negativeTrying = 3;
