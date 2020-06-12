86e3e3085729acfc87a4610df279b2b0b63cedd5
zhernovii <44294650+zhernovii@users.noreply.github.com>
2020-06-12 12:06:39 +0300

    forth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -5,52 +6,58 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-//        System.out.print((char)27+"[30m");
         Scanner scanner = new Scanner(System.in);
-//        int positiveTrying = 5;
-//        int negativeTrying = 3;
         int positiveCounter = 0;
         int negativeCounter = 0;
-        int temporaryCounter;
-        int countTrying = 5;
+        int sizeArray = 5;
+        int sizeItemsOfArray = 3;
 
-        List<String> wrongAnswers = new ArrayList<String>();
-        List<String> listOfExamples = new ArrayList<String>();
+        Integer [][] listOfExamples = new Integer[sizeArray][sizeItemsOfArray];
 
-        for (temporaryCounter = 1; temporaryCounter <= countTrying; temporaryCounter++) {
-            int num1 = getNum();
-            int num2 = getNum();
+        List<String> listOfArrays = new ArrayList<String>();
+
+        for (int temporaryCounter = 0; temporaryCounter < sizeArray; temporaryCounter++) {
+
+            for (int temporaryArrayCounter = 0; temporaryArrayCounter < sizeArray; temporaryArrayCounter++) {
+                int num1 = main.getNum();
+                int num2 = main.getNum();
+                int rightResult = main.multiplication(num1, num2);
+                listOfExamples[temporaryArrayCounter][0]= num1;
+                listOfExamples[temporaryArrayCounter][1]= num2;
+                listOfExamples[temporaryArrayCounter][2]= rightResult;
+            }
+
+
+            int num1 = listOfExamples[temporaryCounter][0];
+            int num2 = listOfExamples[temporaryCounter][1];
+            int rightResult = listOfExamples[temporaryCounter][2];
             System.out.print(num1 + " * " + num2 + " = ");
-            int rightResult = num1 * num2;
+
             int yourResult = scanner.nextInt();
             String doneExample = main.toString(num1, num2, rightResult, yourResult);
+            listOfArrays.add(doneExample);
+
             if (yourResult == rightResult) {
                 System.out.print((char) 27 + "[32mRight");
                 positiveCounter++;
             } else {
                 System.out.print((char) 27 + "[31mWrong" + (char) 27 + "[32m (" + rightResult + ")");
-                wrongAnswers.add(doneExample);
                 negativeCounter++;
             }
-            listOfExamples.add(doneExample);
             System.out.println("\n" + (char) 27 + "[36m" + positiveCounter + "/" + (char) 27 + "[31m" + negativeCounter);
             System.out.println((char) 27 + "[39m");
 
-            if (temporaryCounter >= countTrying) {
+            if (temporaryCounter >= sizeArray-1) {
                 System.out.print("Do you wanna continue?(1 - yes/ 0 - no): ");
                 String askingForQuit = scanner.next();
 
                 while (true) {
                     if (askingForQuit.equals("1")) {
-                        temporaryCounter = 0;
+                        temporaryCounter = -1;
+                        System.out.println("play again");
                         break;
                     } else if (askingForQuit.equals("0")) {
-                        if (negativeCounter > 0) {
-                            System.out.println((char) 27 + "[0mWork again with these examples");
-                            main.workAgainWith(wrongAnswers);
-                        }
-                        System.out.println("----------------");
-                        main.workAgainWith(listOfExamples);
+                        System.out.println("game over");
                         break;
                     } else {
                         System.out.println("You've entered wrong number. Try again");
@@ -60,11 +67,15 @@ public class Main {
             }
         }
 
-        System.out.println("Game over. Your result:" + (char) 27 + "[36m" + positiveCounter + "/" + (char) 27 + "[31m" + negativeCounter);
-
+        System.out.println("Your result:" + (char) 27 + "[36m" + positiveCounter + "/" + (char) 27 + "[31m" + negativeCounter + (char) 27 + "[0m");
+        System.out.println(listOfArrays);
     }
 
-    private static int getNum() {
+    private int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    private int getNum() {
         return (int) (Math.random() * 6 + 4);
     }
 
@@ -76,9 +87,9 @@ public class Main {
 
     public String toString(int num1, int num2, int rightResult, int yourResult) {
         if (yourResult == rightResult){
-            return num1 + " * " + num2 + " = " + rightResult + (char) 27 + "[36m (" + yourResult + ")" + (char) 27 + "[0m";
+            return "\n"+num1 + " * " + num2 + " = " + rightResult + (char) 27 + "[0m";
         } else {
-            return num1 + " * " + num2 + " = " + rightResult + (char) 27 + "[31m (" + yourResult + ")" + (char) 27 + "[0m";
+            return "\n"+num1 + " * " + num2 + " = " + rightResult + (char) 27 + "[31m (" + yourResult + ")" + (char) 27 + "[0m";
         }
     }
 
